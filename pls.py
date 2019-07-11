b2c95b5d43135716b3bbba7b50d076ad86055d31
DrakeLyu <37143032+DrakeLyu@users.noreply.github.com>
2019-07-11 18:07:08 +0800

    Update pls.py

--- pls.py
+++ pls.py
@@ -22,8 +22,9 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from keras.optimizers import Adam
 from sklearn.multioutput import MultiOutputRegressor
-
-
+from sklearn.ensemble import GradientBoostingRegressor
+import xgboost as xgb
+from keras.callbacks import EarlyStopping
 def load_data():
     items = ["nap","pyr","phe"]
     y = {item:[] for item in items}
@@ -50,50 +51,6 @@ def load_data():
         
     return x,y
 
-def ga():
-    Y = []
-    x,y = load_data()
-    for j in range(300):
-        m = []
-        for i in ["nap","pyr","phe"]:
-            m.append( y[i][j])
-        Y.append(m)
-    if True:    
-        x_train, x_test, y_train, y_test = train_test_split( scale(x), Y, test_size=0.1, random_state=4 )
-        x_train = np.array(x_train)
-        x_test = np.array(x_test)
-        y_train = np.array(y_train)
-        y_test = np.array(y_test)
-
-    ##etimator = LinearRegression()
-    estimator = MultiOutputRegressor(SVR(kernel='rbf',gamma='scale', C=1, epsilon=0.2))
-
-    selector = GeneticSelectionCV(estimator,
-                                  cv=5,
-                                  verbose=1,
-                                  scoring="accuracy",
-                                  max_features=5,
-                                  n_population=100,
-                                  crossover_proba=0.5,
-                                  mutation_proba=0.2,
-                                  n_generations=100,
-                                  crossover_independent_proba=0.5,
-                                  mutation_independent_proba=0.05,
-                                  tournament_size=3,
-                                  n_gen_no_change=10,
-                                  caching=True,
-                                  n_jobs=-1)
-    selector = estimator.fit(x_train,y_train[:,0])
-
-    print(selector.support_)
-
-    x_reduced = []
-    for j in range(len(X)):
-        row = []
-        for i in range(len(selector.support_)):
-            if selector.support_[i] ==True:
-                row.append(X[j][i])
-        x_reduced.append(row)
 
 
 def Partial_Least_Squares(x,y):
@@ -218,10 +175,13 @@ def Principal_Components_Regression(x,y):
     
 def PCA_SVR(x,y):
     pca2 = PCA()
+    kpca = KernelPCA(n_components=500, kernel='poly')
     # 10-fold CV, with shuffle
     rmsec = []
     rmsep = []
     rmsecv = []
+    maep = []
+    maec = []
     Y = []
     for j in range(300):
         m = []
@@ -239,32 +199,63 @@ def PCA_SVR(x,y):
 
         n =300
 
-        svr = MultiOutputRegressor(SVR(kernel='rbf',gamma='scale', C=1, epsilon=0.2))
+        #svr = MultiOutputRegressor(GradientBoostingRegressor(n_estimators=200, learning_rate=0.1,max_depth=2, random_state=0, loss='ls'))
+        svr = MultiOutputRegressor(SVR(kernel='sigmoid',gamma='scale', C=1, epsilon=0.2))
+
         svr.fit(x_train[:,:n], y_train)
         
         pred0 = svr.predict(x_train[:,:n] )        
         pred = svr.predict(x_test[:,:n])
-        print(pred)
-        for i in range(len(pred)):
-            for j in range(0,3):
-                if pred[i][j] < 2:
-                    pred[i][j] = 0
+
+##        for i in range(len(pred)):
+##            for j in range(0,3):
+##                if pred[i][j] < 2:
+##                    pred[i][j] = 0
 
         for i in range(len(y_test)):
             print(y_test[i],pred[i])
             
         for i in range(0,3):
             mse0 = mean_squared_error(y_test[:,i], pred[:,i])
+            mae0 = mean_absolute_error(y_test[:,i], pred[:,i])
             rmsep.append(math.sqrt(mse0))
-
+            maep.append(mae0)
             
         for i in range(0,3):
             mse1 = mean_squared_error(y_train[:,i], pred0[:,i])
+            mae1 = mean_absolute_error(y_train[:,i], pred0[:,i])
             rmsec.append(math.sqrt(mse1))
+            maec.append(mae1)
             
     print("rmsec",rmsec)
     #print("rmsecv",rmsecv)
     print("rmsep",rmsep)
+    print("maec",maec)
+    print("maep",maep)
+    print(len(pred))
+    m = np.arange(0,30)
+
+    fig, ax = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+    plt.ylabel('Concentration/ppb')    
+    ax[0].scatter(m,pred[:,0],c='tab:blue',label='y_predict')
+    ax[0].scatter(m,y_test[:,0],c='tab:orange',label='y_test')
+    ax[0].vlines(m,[0],y_test[:,0])
+    ax[0].set_title('BaP')
+    ax[0].legend()
+    ax[1].scatter(m,pred[:,1],c='tab:blue',label='y_predict')
+    ax[1].scatter(m,y_test[:,1],c='tab:orange',label='y_test')
+    ax[1].vlines(m,[0],y_test[:,1])
+    ax[1].set_title('PHE')
+    ax[1].legend()
+    ax[2].scatter(m,pred[:,2],c='tab:blue',label='y_predict')
+    ax[2].scatter(m,y_test[:,2],c='tab:orange',label='y_test')
+    ax[2].vlines(m,[0],y_test[:,2])
+    ax[2].set_title('PYR')
+    ax[2].legend()
+    fig.suptitle('Predicted value and true value')
+
+    #ax.grid(True)
+    plt.show()
 
         # Calculate MSE using CV for the 19 principle components, adding one component at the time.
 
@@ -353,6 +344,8 @@ def ANN(x,y):
     rmsec = []
     rmsep = []
     rmsecv = []
+    maep = []
+    maec = []
     Y = []
     for j in range(300):
         m = []
@@ -364,43 +357,40 @@ def ANN(x,y):
         x_reduced = pca2.fit_transform(scale(x))
         #x_train, x_test, y_train, y_test = train_test_split( scale(x), Y, test_size=0.1, random_state=4 )
 
-        x_train, x_test, y_train, y_test = train_test_split( x_reduced[:,:300],Y , test_size=0.15, random_state=4 )
+        x_train, x_test, y_train, y_test = train_test_split( x_reduced[:,:250],Y , test_size=0.15, random_state=4 )
         x_train = np.array(x_train)
         x_test = np.array(x_test)
         y_train = np.array(y_train)
         y_test = np.array(y_test)
         n = len(x_train)
-
-        
+        earlystop = EarlyStopping(monitor='val_acc', min_delta=0.1, patience=50,verbose=1, mode='auto')
+        callbacks_list = [earlystop]       
         network = models.Sequential()
 
         neurons = x_train.shape[1]
 
-        network.add(layers.Dense(neurons*3, activation='relu', input_shape=(neurons,)))
-
+        network.add(layers.Dense(neurons*2, activation='relu', input_shape=(neurons,)))
+        network.add(BatchNormalization())
+        
         network.add(layers.Dense(neurons*2, activation='relu'))
-        #network.add(BatchNormalization())
-        network.add(Dropout(0.5))
+        network.add(BatchNormalization())
         
         network.add(layers.Dense(neurons, activation='relu'))
-        #network.add(BatchNormalization())
+        network.add(BatchNormalization())
+        
         network.add(layers.Dense(neurons, activation='relu'))
-        #network.add(BatchNormalization())
-        network.add(Dropout(0.5))
+        network.add(BatchNormalization())
         
         network.add(layers.Dense(neurons//2, activation='relu'))
-        #network.add(BatchNormalization())        
-        network.add(layers.Dropout(0.5))
+        network.add(BatchNormalization())        
 
         network.add(layers.Dense(3, activation='relu'))
-
-        #categorical_crossentropy
 
         adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
         network.compile(optimizer=adam,loss='mean_squared_error',metrics=['accuracy'])
         #network.compile(optimizer = adam,loss='mean_absolute_error',metrics=metrics=[metrics.mae])
         network.summary()
-        history = network.fit(x_train, y_train, validation_split=0.1, epochs=200, batch_size=64)
+        history = network.fit(x_train, y_train, validation_split=0.1, epochs=300, batch_size=64,callbacks=callbacks_list)
 
         pred0 = network.predict(x_train )        
         pred = network.predict(x_test)
@@ -408,22 +398,69 @@ def ANN(x,y):
             print(y_test[i],pred[i])
             
         for i in range(0,3):
-
             mse0 = mean_squared_error(y_test[:,i], pred[:,i])
+            mae0 = mean_absolute_error(y_test[:,i], pred[:,i])
             rmsep.append(math.sqrt(mse0))
-
+            maep.append(mae0)
             
         for i in range(0,3):
-               
             mse1 = mean_squared_error(y_train[:,i], pred0[:,i])
+            mae1 = mean_absolute_error(y_train[:,i], pred0[:,i])
             rmsec.append(math.sqrt(mse1))
+            maec.append(mae1)
+            
+        print("rmsec",rmsec)
+        #print("rmsecv",rmsecv)
+        print("rmsep",rmsep)
+        print("maec",maec)
+        print("maep",maep)
+        print(len(pred))
 
-        #score = -1*model_selection.cross_val_score(network, scale(x_train), y_train.ravel(), cv=kf_10, scoring='neg_mean_squared_error').mean()    
-        #rmsecv.append(math.sqrt(score))
-    print("rmsec",rmsec)
-    #print("rmsecv",rmsecv)
-    print("rmsep",rmsep)
-    
+    def error_plot():
+        m = np.arange(0,45)
+
+        fig, ax = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+        plt.ylabel('Concentration/ppb')    
+        ax[0].scatter(m,pred[:,0],c='tab:blue',label='y_predict')
+        #ax[0].scatter(m,y_test[:,0],c='tab:orange',label='y_test')
+        ax[0].vlines(m,[0],y_test[:,0],label='y_test')
+        ax[0].set_title('BaP')
+        ax[0].legend()
+        ax[1].scatter(m,pred[:,1],c='tab:blue',label='y_predict')
+        #ax[1].scatter(m,y_test[:,1],c='tab:orange',label='y_test')
+        ax[1].vlines(m,[0],y_test[:,1],label='y_test')
+        ax[1].set_title('PHE')
+        ax[1].legend()
+        ax[2].scatter(m,pred[:,2],c='tab:blue',label='y_predict')
+        #ax[2].scatter(m,y_test[:,2],c='tab:orange',label='y_test')
+        ax[2].vlines(m,[0],y_test[:,2],label='y_test')
+        ax[2].set_title('PYR')
+        ax[2].legend(loc='upper right')
+        fig.suptitle('Predicted value and true value')
+
+        #ax.grid(True)
+        plt.show()
+
+    def training_plot():
+            # Plot training & validation accuracy values
+            plt.plot(history.history['acc'])
+            plt.plot(history.history['val_acc'])
+            plt.title('Model accuracy')
+            plt.ylabel('Accuracy')
+            plt.xlabel('Epoch')
+            plt.legend(['Train', 'Test'], loc='upper left')
+            plt.show()
+
+            # Plot training & validation loss values
+            plt.plot(history.history['loss'])
+            plt.plot(history.history['val_loss'])
+            plt.title('Model loss')
+            plt.ylabel('Loss')
+            plt.xlabel('Epoch')
+            plt.legend(['Train', 'Test'], loc='upper left')
+            plt.show()
+    error_plot()            
+    training_plot()
 ##rmsec [0.8868594169441504, 0.4694611642639408, 0.7505665612748366]
 ##rmsep [1.8627601181173121, 0.9917311552473371, 1.7557357197799104]
 ##pca:
@@ -446,7 +483,7 @@ def CNN(x,y):
     x_reduced = pca2.fit_transform(scale(x))
     #x_train, x_test, y_train, y_test = train_test_split( x, y[i], test_size=0.1, random_state=4 )
 
-    x_train, x_test, y_train, y_test = train_test_split( x_reduced[:,:300], Y, test_size=0.15, random_state=4 )
+    x_train, x_test, y_train, y_test = train_test_split( x, Y, test_size=0.15, random_state=4 )
     x_train = np.array(x_train)
     x_test = np.array(x_test)
     y_train = np.array(y_train)
@@ -496,6 +533,6 @@ kf_10 = model_selection.KFold(n_splits=10, shuffle=True, random_state=1)
 #Partial_Least_Squares(x,y)
 #Principal_Components_Regression(x,y)
 #Support_Vector_Regression(x,y)
-PCA_SVR(x,y)
-#ANN(x,y)
+#PCA_SVR(x,y)
+ANN(x,y)
 #CNN(x,y)
