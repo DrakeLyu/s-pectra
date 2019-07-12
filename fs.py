3b329654087f7cff363bfec40ac85a96ec1d3ef1
DrakeLyu <37143032+DrakeLyu@users.noreply.github.com>
2019-07-12 17:29:55 +0800

    Update fs.py

--- fs.py
+++ fs.py
@@ -1,146 +1,59 @@
-from __future__ import print_function
-import numpy as np
-from sklearn import datasets, linear_model
-import re,math
-from sklearn.linear_model import LinearRegression
-from genetic_selection import GeneticSelectionCV
-from sklearn.svm import SVR
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import scale
-from sklearn.feature_selection import VarianceThreshold
 
-def load_data():
-    wl = []
-    items = ["nap","pyr","phe"]
-    y = {item:[] for item in items}
-    x =[]
-    Y = []
-    path = "C:/Users/jiali/Desktop/clean_data.csv"
-    
-    file = open(path, 'r',newline='')
-    line = file.readline()
+import argparse
+import re
+import csv
 
-    for i in re.split(',',line)[7:]:
-        wl.append(float(i.strip('\n')))
+path = "C:/Users/jiali/Downloads/s-pectra-master/s-pectra-master/cfm_pair.csv"
+f = open(path, 'r')
 
-    for line in file:
-        #label = []
-        data = re.split(',',line)
-        for i in range(len(data)):
-            data[i] = float(data[i])
-            if i == 2:
-                y["nap"].append(data[2])
-            if i == 4:
-                y["pyr"].append(data[4])             
-            if i == 6:
-                y["phe"].append(data[6])
+weight = []
+molecule = []
+intensity = []
+energy = [0,1,2]
+##molecule = {k:[] for k in mol}
 
-        x.append( data[7:] )
-    for j in range(300):
-        m = []
-        for i in ["nap","pyr","phe"]:
-            m.append( y[i][j])
-        Y.append(m)
-    #sel = VarianceThreshold(threshold=100000)
-    #x_vt = sel.fit_transform(x)
+f.readline()
+lines = f.readlines()
 
-    return x,y, Y,wl
+for line in lines:
+    mol = line.strip().split(',')[0]
+    ins = line.strip().split(',')[3]
+    we = line.strip().split(',')[4]
+    if mol not in molecule:
+        molecule.append(mol)
+    if we not in weight:
+        weight.append(we)
+    if ins not in intensity:
+        intensity.append(ins)
 
-def ga():
+num_molecule = 4
+ins_tuple = {k:[] for k in range(num_molecule*3)}  
+wei_tuple = {k:[] for k in range(num_molecule*3)}        
+for line in lines:
+    mo = line.strip().split(',')[0]
+    en = line.strip().split(',')[1]
+    ins = line.strip().split(',')[3]
+    we = line.strip().split(',')[4]
 
-    x,y,Y = load_data()
-    index = []
+    for num_mol,mol in enumerate(molecule):
+        for num_ene,ene in enumerate(energy):
+            if mo == mol and en == str(ene):
+                ins_tuple[3*num_mol+num_ene].append(ins)
+                wei_tuple[3*num_mol+num_ene].append(we)
+print(ins_tuple)
+print(wei_tuple)
+num_ins = len(ins_tuple[0])
+for i in range(num_molecule*3):
 
-    for i in ["nap","pyr","phe"]:
-        estimator = LinearRegression()
-        ##estimator = SVR(kernel='rbf',gamma='scale', C=1, epsilon=0.2)
-        selector = GeneticSelectionCV(estimator,
-                                      cv=5,
-                                      verbose=1,
-                                      scoring="accuracy",
-                                      max_features=5,
-                                      n_population=100,
-                                      crossover_proba=0.5,
-                                      mutation_proba=0.2,
-                                      n_generations=100,
-                                      crossover_independent_proba=0.5,
-                                      mutation_independent_proba=0.05,
-                                      tournament_size=3,
-                                      n_gen_no_change=10,
-                                      caching=True,
-                                      n_jobs=-1)
-        selector = selector.fit(x,y[i])
+    for j in range(num_ins-1):
 
-        print(selector.support_)
+        for we in weight:
+            if wei_tuple[i][j] < we < wei_tuple[i][j+1]:
+                wei_tuple[i].insert( j,we )
+                ins_tuple[i].insert( j,'0' )
+print(ins_tuple)
+print(wei_tuple)
+##plt.plot(t, s)
+##plt.show()
+f.close()
 
-        for j in range(len(selector.support_)):
-            if selector.support_[j] ==True:
-                if j not in index:
-                    index.append(j)
-    index.sort()
-    
-    x_reduced = []
-    for i in range(len(x)):
-        row = []
-        for j in range(len(x[0])):
-            if j in index:
-                row.append(x[i][j])
-        x_reduced.append(row)
-    print(len(x_reduced))
-    print(len(x_reduced[0]))
-
-    return x,x_reduced,Y
-
-def svr(x,y):
-    rmsec = []
-    rmsep = []
-    x_train, x_test, y_train, y_test = train_test_split( scale(x), Y, test_size=0.1, random_state=4 )
-    x_train = np.array(x_train)
-    x_test = np.array(x_test)
-    y_train = np.array(y_train)
-    y_test = np.array(y_test)
-    ##        
-    svr = MultiOutputRegressor(SVR(kernel='rbf',gamma='scale', C=1, epsilon=0.2))
-    svr.fit(x_train, y_train)
-    pred0 = svr.predict(x_train)
-    pred = svr.predict(x_test) 
-    ##        
-    ##
-    ##    for i in range(len(pred)):
-    ##        for j in range(0,3):
-    ##            if pred[i][j] < 2:
-    ##                pred[i][j] = 0
-    ##
-    ##    for i in range(len(y_test)):
-    ##        print(y_test[i],pred[i])
-
-    for i in range(0,3):
-        mse0 = mean_squared_error(y_test[:,i], pred[:,i])
-        rmsep.append(math.sqrt(mse0))
-
-                
-    for i in range(0,3):
-        mse1 = mean_squared_error(y_train[:,i], pred0[:,i])
-        rmsec.append(math.sqrt(mse1))
-    ##            
-    print("rmsec",rmsec)
-    ##    #print("rmsecv",rmsecv)
-    print("rmsep",rmsep)
-
-##x,x_reduced,Y = ga()
-##svr(x,Y)
-##svr(x_reduced,Y)
-import matplotlib.pyplot as plt
-
-x,y,Y,wl = load_data()
-
-fig,ax1 = plt.subplots()
-ax1.plot(wl,x[0])
-ax1.set_xlim(350,1800)
-ax1.set_xlabel('wavelenth')
-ax1.set_ylabel('s1 and s2')
-ax1.grid(True)
-
-plt.show()
